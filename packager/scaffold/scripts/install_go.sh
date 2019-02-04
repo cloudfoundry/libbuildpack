290dcfc73c7da2851c7abfe442162153e00a9cb5
Zander Mackie <amackie@pivotal.io>
2019-02-04 17:18:21 -0500

    Update go to 1.11.5 for install_go
    [#163701450]

--- packager/scaffold/scripts/install_go.sh
+++ packager/scaffold/scripts/install_go.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
+
 set -euo pipefail
 
-GO_VERSION="1.10"
+GO_VERSION="1.11.5"
+
+if [ $CF_STACK == "cflinuxfs2" ]; then
+    GO_SHA256="51cab63f3de5e2f75a9036801712e4d7ae9bf226f0b61abce8d784e698148d3b"
+elif [ $CF_STACK == "cflinuxfs3" ]; then
+    GO_SHA256="ee770df4e1863ee8e07574cb48e0245b61bec8f118faf6ec3742ea89eb20db28"
+fi
+
 export GoInstallDir="/tmp/go$GO_VERSION"
 mkdir -p $GoInstallDir
 
 if [ ! -f $GoInstallDir/go/bin/go ]; then
-GO_SHA256="244200952f414e9ae6269d32569722a7cd88435f5c52d488cd9599b8bfa1498b"
-URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${GO_SHA256:0:8}.tar.gz
+  URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${CF_STACK}-${GO_SHA256:0:8}.tar.gz
 
-echo "-----> Download go ${GO_VERSION}"
-curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
+  echo "-----> Download go ${GO_VERSION}"
+  curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
 
-DOWNLOAD_SHA256=$(shasum -a256 /tmp/go.tar.gz | cut -d ' ' -f 1)
-if [[ $DOWNLOAD_SHA256 != $GO_SHA256 ]]; then
-echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $GO_SHA256"
-exit 1
-fi
+  DOWNLOAD_SHA256=$(shasum -a 256 /tmp/go.tar.gz | cut -d ' ' -f 1)
 
-tar xzf /tmp/go.tar.gz -C $GoInstallDir
-rm /tmp/go.tar.gz
+  if [[ $DOWNLOAD_SHA256 != $GO_SHA256 ]]; then
+    echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $GO_SHA256"
+    exit 1
+  fi
+
+  tar xzf /tmp/go.tar.gz -C $GoInstallDir
+  rm /tmp/go.tar.gz
 fi
 if [ ! -f $GoInstallDir/go/bin/go ]; then
-echo "       **ERROR** Could not download go"
-exit 1
+  echo "       **ERROR** Could not download go"
+  exit 1
 fi
