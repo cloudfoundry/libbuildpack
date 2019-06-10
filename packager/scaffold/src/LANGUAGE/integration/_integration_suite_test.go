f7521fdfa004c6c3bf913be9a6e7779561ed57a9
Dr Nic Williams <drnicwilliams@gmail.com>
2019-06-11 08:48:34 +1000

    update _integration_suite_test.go scaffold for underlying API changes

--- packager/scaffold/src/LANGUAGE/integration/_integration_suite_test.go
+++ packager/scaffold/src/LANGUAGE/integration/_integration_suite_test.go
@@ -34,7 +34,7 @@ func init() {
 var _ = SynchronizedBeforeSuite(func() []byte {
 	// Run once
 	if buildpackVersion == "" {
-		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack("") // "" denotes any stack. Use specific stack (e.g. "cflinuxfs2" if desired)
+		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack("", false) // stackAssociationSupported=false denotes any stack. Set to true and specific stack (e.g. "cflinuxfs2" if desired)
 		Expect(err).NotTo(HaveOccurred())
 
 		data, err := json.Marshal(packagedBuildpack)
@@ -120,7 +120,7 @@ func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
 			Expect(err).To(BeNil())
 			defer os.Remove(bpFile)
 
-			traffic, built, err := cutlass.InternetTraffic(
+			traffic, built, logs, err := cutlass.InternetTraffic(
 				bpDir,
 				filepath.Join("fixtures", fixtureName),
 				bpFile,
@@ -128,6 +128,7 @@ func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
 			)
 			Expect(err).To(BeNil())
 			Expect(built).To(BeTrue())
+			Expect(logs).To(BeEmpty())
 
 			destUrl, err := url.Parse(proxy.URL)
 			Expect(err).To(BeNil())
@@ -151,7 +152,7 @@ func AssertNoInternetTraffic(fixtureName string) {
 		Expect(err).To(BeNil())
 		defer os.Remove(bpFile)
 
-		traffic, built, err := cutlass.InternetTraffic(
+		traffic, built, logs, err := cutlass.InternetTraffic(
 			bpDir,
 			filepath.Join("fixtures", fixtureName),
 			bpFile,
@@ -160,5 +161,6 @@ func AssertNoInternetTraffic(fixtureName string) {
 		Expect(err).To(BeNil())
 		Expect(built).To(BeTrue())
 		Expect(traffic).To(BeEmpty())
+		Expect(logs).To(BeEmpty())
 	})
 }
